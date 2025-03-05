b5bd98c7f07261741372d6520da964c32ec66b57
Arda Aslan <ardaaslan28arda@gmail.com>
2025-03-05 23:35:50 +0300

    first commit

---
+++ Dogu_Karadeniz_Odev/Dogu_Karadeniz_Odev.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE* dosya;
+	int j, trafikKodu, okutulanTrafikKodu=0, bulunanTrafikKodu=0;
+	char sehir[30];
+
+	dosya = fopen("Sehir-Kod.txt", "r"); //Dosyayý Acma
+
+	printf("Okutulan sehir kodu?"); //Sehir kodunun okutulmasi
+	scanf("%d",&okutulanTrafikKodu);
+
+	while (fscanf(dosya, "%s %d\n", sehir, &trafikKodu) != EOF)
+	{
+		if (okutulanTrafikKodu==trafikKodu) //Dosyanin icerisinde sehir kodununu bulmaya calýsýyor
+		{
+			bulunanTrafikKodu = trafikKodu; //Bulursa buldugumuz sehir kodunu baska bir degiskene aktarýyor
+		}
+	}
+
+	if (bulunanTrafikKodu!=0) //az onceki aktarma yapilmis mi yoksa yapilmamis mi diye kontrol ediyor
+	{
+		fseek(dosya, 0, SEEK_SET);
+		while (fscanf(dosya, "%s %d\n", sehir, &trafikKodu) != EOF)
+		{
+			if (okutulanTrafikKodu == trafikKodu) // if (bulunanTrafikKodu == trafikKodu) tercih edilebilir
+			{
+				printf("\n%s Dogu Karadeniz bolgesinde yer alir", sehir);
+			}
+		}
+	}
+	else //aktarma yapilmamissa:
+	{
+		printf("\nHatali Kod Girdiniz");
+	}
+	fclose(dosya); //Dosyanin kapanmasi
+
+	return (EXIT_SUCCESS);
+}
